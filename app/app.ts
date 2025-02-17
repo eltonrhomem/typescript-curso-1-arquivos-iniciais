c16ade08f9bd5ca7a94f3a3c1579a7fa721cd5c7
F2872541 Elton Rodrigues Homem <elton.homem@bb.com.br>
2025-02-17 13:01:47 -0300

    fazendo heranças e classes abstratas

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,5 @@
 import { NegociacaoController } from "./controllers/negociacao-controller.js";
+import { NegociacoesView } from "./views/negociacoes-views.js";
 
 
 const controller = new NegociacaoController();
@@ -7,3 +8,7 @@ form.addEventListener('submit',   (event: Event) => {
     event.preventDefault();
     controller.adiciona();
 });
+
+const negociacoesView = new NegociacoesView('#negociacoesView');
+// const template = negociacoesView.template();
+// console.log(template);
